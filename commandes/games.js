ec313acaf2fe946f75cb101c227652154bdd0729
🦅NEXA📜 <144521464+henrygokugt99@users.noreply.github.com>
2024-06-20 09:54:17 +0300

    🔥DAIMA🔥

--- commandes/games.js
+++ commandes/games.js
@@ -142,7 +142,7 @@ Therefore, the game is canceled`, mentions : [auteurMessage, auteurMsgRepondu]})
 
 
 zokou(
-    { nomCom: "quizz", categorie: "Games", reaction: "👨🏿‍💻" },
+    { nomCom: "quizz", categorie: "Games", reaction: "🏂" },
     async (origineMessage, zk, commandeOptions) => {
         const { repondre, auteurMessage } = commandeOptions;
 
