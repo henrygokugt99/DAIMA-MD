caf389e51b625631cb03e088da53d63c5c272a26
🦅NEXA📜 <144521464+henrygokugt99@users.noreply.github.com>
2024-06-20 09:26:35 +0300

    🔥DAIMA🔥

--- commandes/Tempmail.js
+++ commandes/Tempmail.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "tempmail", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*𝑴𝑬𝑮𝑨𝑻𝑹𝑶𝑵-𝑩𝑶𝑻* Temporary emails,Powered by *𝑲𝒊𝒎𝒎𝒚* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by *𝑴𝑬𝑮𝑨𝑻𝑹𝑶𝑵.*";
+    let z = '*ᗫᗅᓿᙢᗅ ᙢᗫ* Temporary emails,Powered by *𝑲𝒊𝒎𝒎𝒚* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by *ᗫᗅᓿᙢᗅ.*";
     let d = ' https://tempmailpro.app';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/bcf5cec3445652ab197bc.jpg';
