8f2a7a92210385dee1edafd4c590aee9593b83e2
🦅NEXA📜 <144521464+henrygokugt99@users.noreply.github.com>
2024-06-20 10:58:40 +0300

    🔥DAIMA🔥

--- framework/mesfonctions.js
+++ framework/mesfonctions.js
@@ -32,7 +32,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'FLASH-MD',
+        pack: 'DAIMA-MD',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
