aba39d155b62f8ab063416817d62f859b9c54a0f
🦅NEXA📜 <144521464+henrygokugt99@users.noreply.github.com>
2024-06-20 10:05:58 +0300

    🔥DAIMA🔥

--- commandes/groupe.js
+++ commandes/groupe.js
@@ -31,7 +31,7 @@ zokou({ nomCom: "tagall", categorie: 'Group', reaction: "📣" }, async (dest, z
   let membresGroupe = verifGroupe ? await infosGroupe.participants : ""
   var tag = ""; 
   tag += `========================\n  
-        ✞ *MEGATRON-BOT* ✞
+        ⚚ *ƊƛƖMƛ MƊ* ⚚
 ========================\n
 👥 Group : ${nomGroupe} 🚀 
 👤 Author : *${nomAuteurMessage}* 👋 
