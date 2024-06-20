8aa1750d9e914056936697234667a058767edb5b
🦅NEXA📜 <144521464+henrygokugt99@users.noreply.github.com>
2024-06-20 09:25:15 +0300

    🔥DAIMA🔥

--- commandes/Mods.js
+++ commandes/Mods.js
@@ -110,7 +110,7 @@ fs.unlinkSync(medis)
 
           const sticker = new Sticker(buffer.data, {
             pack: nomAuteurMessage,
-            author: "༒𝐃𝚫𝚳𝚯𝚴𖤍༒",
+            author: "༒ᗫᗅᓿᙢᗅ ᙢᗫ𖤍༒",
             type: StickerTypes.FULL,
             categories: ['🤩', '🎉'],
             id: '12345',
@@ -253,7 +253,7 @@ zokou({ nomCom: "kickall", categorie: 'Group', reaction: "📣" }, async (dest, 
   const metadata = await zk.groupMetadata(dest) ;
 
 
-  if (!verifGroupe) { repondre("✋🏿 ✋🏿this command is reserved for groups ❌"); return; }
+  if (!verifGroupe) { repondre("👥 this command is reserved for groups ❌"); return; }
   if (superUser || auteurMessage == metadata.owner) { 
 
    repondre('No_admin members will be removed from the group. You have 5 seconds to reclaim your choice by restarting the bot.') ;
@@ -527,7 +527,7 @@ zokou({ nomCom: "save", categorie: "OWNER" }, async (dest, zk, commandeOptions) 
           let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.stickerMessage)
 
           let stickerMess = new Sticker(media, {
-            pack: '𝐌𝐄𝐆𝐀𝐓𝐑𝐎𝐍-𝐁𝐎𝐓',
+            pack: 'ᗫᗅᓿᙢᗅ ᙢᗫ',
             type: StickerTypes.CROPPED,
             categories: ["🤩", "🎉"],
             id: "12345",
@@ -579,7 +579,7 @@ zokou({
 
   if (alldata.length === 0 ) { repondre(`To activate or modify the mention; follow this syntax: mention link type message
   The different types are audio, video, image, and sticker.
-  Example: mention https://static.animecorner.me/2023/08/op2.jpg image Hi, my name is King G`) ; return}
+  Example: mention https://static.animecorner.me/2023/08/op2.jpg image Hi, my name is ₦ɆӾ₳▂ ▄ ▅ ▆ ▇ █ 2`) ; return}
 
       if(data.status == 'non') {
           etat = 'Desactived'
@@ -600,7 +600,7 @@ Link: ${url}
 
 To activate or modify the mention, follow this syntax: mention link type message
 The different types are audio, video, image, and sticker.
-Example: mention https://static.animecorner.me/2023/08/op2.jpg image Hi, my name is King G 
+Example: mention https://static.animecorner.me/2023/08/op2.jpg image Hi, my name is ₦ɆӾ₳▂ ▄ ▅ ▆ ▇ █
 
 To stop the mention, use mention stop`;
 
