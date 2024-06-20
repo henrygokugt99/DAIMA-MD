5e00f8b5654359ed0cb6d0c6633dfce83436581a
🦅NEXA📜 <144521464+henrygokugt99@users.noreply.github.com>
2024-06-20 10:28:04 +0300

    🔥DAIMA🔥

--- commandes/stickersearch.js
+++ commandes/stickersearch.js
@@ -33,7 +33,7 @@ async (dest, zk, commandeOptions) => {
 
     const stickerMess = new Sticker(gifUrl, {
       pack: packname,
-      author: 'MEGATRON-BOT',
+      author: 'ƊƛƖMƛ MƊ',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
