fa68479a4b9b9b4b1e3b61b4da5c4bf5d96baab2
🦅NEXA📜 <144521464+henrygokugt99@users.noreply.github.com>
2024-06-24 07:03:54 +0300

    Update app.js

--- framework/app.js
+++ framework/app.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.reagir = void 0;
 async function reagir(dest, zok, msg, emoji) {
-    await zok.sendMessage(dest, { react: { text: emoji, key: msg.key } });
+    await zok.sendMessage(dest, { react: { text: emoji, key: ms.key } });
 }
 exports.reagir = reagir;
